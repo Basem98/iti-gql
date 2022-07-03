38555fef330578d5388441e128579aa5b9ad4b4c
Basem98 <basem.mostafa98@hotmail.com>
2022-07-03 19:04:38 +0200

    refactor: Move schedma definition to a separate file

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
 const { ApolloServer, gql } = require('apollo-server');
-const resolvers = require('./resolver');
+const resolvers = require('./resolvers/index.js');
 const PostsDatabase = require('./datasource');
 const config = require('./config/envConfig');
-
-/* -------------- Define the data types used throughout the server -------------- */
-const typeDefs = gql`
-  type Comment {
-    author: String
-    body: String
-  }
-
-  type Post {
-    id: ID!
-    title: String
-    author: String
-    body: String
-    comments: [Comment]
-  }
-
-  type Query {
-    post(id: Int!): Post
-    posts: [Post]
-  }
-
-  type Mutation {
-    createPost(id: Int!, title: String!, author: String!, body: String): Post
-    updatePost(id: Int!, title: String, author: String, body: String): Post
-    deletePost(id: Int!): [Post]
-    addCommentToPost(id: Int!, author: String!, body: String!): Post
-    updateCommentByAuthor(id: Int!, author: String!, body: String!): Post
-    deleteCommentByAuthor(id: Int!, author: String!): Post
-  }
-`;
+const fs = require('fs');
+const path = require('path');
+const typeDefs = gql(fs.readFileSync(path.resolve(__dirname, 'schema.graphql'), 'utf-8'));
 
 
 /* -------------- Configure the Apollo server -------------- */
