1bbd631cbaa792f4ce124e982b7ea06e8a9fab55
Basem98 <basem.mostafa98@hotmail.com>
2022-07-03 12:44:19 +0200

    feat: Add Comment type to schema

--- dataset.js
+++ dataset.js
@@ -3,13 +3,23 @@ const posts = [
     'id': 1,
     title: 'The Awakening',
     author: 'Kate Chopin',
-    body: 'Post body 1'
+    body: 'Post body 1',
+    comments: [
+      {author: 'Chris Master', body: 'Comment 1'},
+      {author: 'Paul Dano', body: 'Comment 2'},
+      {author: 'Jessica Rudd', body: 'Comment 3'}
+    ]
   },
   {
     'id': 2,
     title: 'City of Glass',
     author: 'Paul Auster',
-    body: 'Post body 2'
+    body: 'Post body 2',
+    comments: [
+      {author: 'Chris Master', body: 'Comment 1'},
+      {author: 'Paul Dano', body: 'Comment 2'},
+      {author: 'Jessica Rudd', body: 'Comment 3'}
+    ]
   },
 ];
 
--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const resolvers = require('./resolver');
 
 /* -------------- Define the data types used throughout the server -------------- */
 const typeDefs = gql`
+  type Comment {
+    author: String
+    body: String
+  }
+
   type Post {
     id: ID!
     title: String
     author: String
     body: String
+    comments: [Comment]
   }
 
   type Query {
@@ -19,6 +25,7 @@ const typeDefs = gql`
     createPost(id: Int, title: String, author: String, body: String): Post
     updatePost(id: Int, title: String, author: String, body: String): Post
     deletePost(id: Int): [Post]
+    addCommentToPost(id: Int, author: String, body: String): Post
   }
 `;
 
